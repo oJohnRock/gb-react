2e03065003142735ec95a32457d5e23be4b23ba4
m.timurshin <ojohnrock@gmail.com>
2022-03-26 20:49:41 +0300

    Задание 3

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import Message from './Message';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const messageText = 'Урок 1. Знакомство с ReactJS. Первые компоненты';
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <Message messageText={messageText} />
+            </header>
+        </div>
+    );
 }
 
 export default App;
