2ab228f6e09cc0487f1d1b48931505696372b900
m.timurshin <m.timurshin@ittechnogroup.ru>
2022-03-31 16:34:42 +0300

    Задание 2

--- src/App.js
+++ src/App.js
@@ -5,12 +5,26 @@ import Message from "./Message";
 function App() {
     const messageText = "Урок 2. Жизненный цикл компонента";
 
-    const [messageList, setMessageList] = useState([]);
+    const [messageList, setMessageList] = useState([
+        { author: "max", text: "qweqweqweqwqweqwe" },
+        { author: "lex", text: "mmlmblngln" },
+    ]);
+    const list = messageList.map((el) => {
+        return (
+            <>
+                <li className="message__item">
+                    <h4 className="message__author">{el.author}:</h4>
+                    <p className="message__text">{el.text}</p>
+                </li>
+            </>
+        );
+    });
 
     return (
         <div className="App">
             <header className="App-header">
                 <Message messageText={messageText} />
+                <ul className="message__list">{list}</ul>
             </header>
         </div>
     );
